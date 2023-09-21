6928fd5ad2149a690ce824eaf14a38898f9ab4f4
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-09-21 14:32:43 +0600

    add some css

---
+++ script.js
@@ -0,0 +1,32 @@
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+
+
+let sideMenuIcon = document.getElementById("side-menu");
+let sideBar = document.getElementById("side-bar");
+let sideLinks = sideBar.querySelectorAll('li');
+
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
+});
+
+sideMenuIcon.addEventListener('click', ()=>{
+  sideBar.classList.toggle('activeSidebar');
+  sideBar.style.display= 'block'
+  
+})
+sideLinks.forEach((sideLinks)=>{
+  sideLinks.addEventListener('click', ()=>{
+    sideBar.classList.remove('activeSidebar');
+    
+  })
+})
+
